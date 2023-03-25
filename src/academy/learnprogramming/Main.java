fe7694b859fe969a5f6cde30efeda2194891dfa5
shubh9shr8 <122762929+shubh9shr8@users.noreply.github.com>
2023-03-25 12:41:27 +0530

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,29 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int count = 0;
+            for (int i = 10; i<50; i++) {
+                if (isPrime(i)) {
+                    System.out.println("number " + i + " is a prime number");
+                    count++;
+                    if (count ==3) {
+                        System.out.println("found 3 - exiting the loop");
+                        break;
+                    }
+                }
+            }
+                }
+        public static boolean isPrime(int wholeNumber) {
+            if (wholeNumber <= 2) {
+                return (wholeNumber == 2);
+            }
+            for (int divisor = 2; divisor < wholeNumber; divisor++) {
+                if (wholeNumber % divisor == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
